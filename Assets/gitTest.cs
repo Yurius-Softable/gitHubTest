2903258c07a5f503258b6a02fc58b8669c9e08a8
Yurius-Softable <yurius.softable@gmail.com>
2020-01-31 15:11:07 +0300

    написал хз

--- Assets/gitTest.cs
+++ Assets/gitTest.cs
@@ -7,7 +7,7 @@ public class gitTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Хз");
     }
 
     // Update is called once per frame
